1b9e167acb89178e3e99af51152985dd76347b05
tejaswinijakka <tejaswinijakka6@gmail.com>
2020-10-31 12:55:57 +0530

    Adding everything

---
+++ second/polls/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+urlpatterns = [ path('index',views.index,name='index'),
+https://admintesting.herokuapp.com/addfood('index',views.index,name='index'),
+https://admintesting.herokuapp.com/removefood('index',views.index,name='index'),
+https://admintesting.herokuapp.com/addevents('index',views.index,name='index'),
+https://admintesting.herokuapp.com/deleteevents('index',views.index,name='index'),
+https://admintesting.herokuapp.com/lostfound('index',views.index,name='index'),
+https://admintesting.herokuapp.com/rlostfound('index',views.index,name='index'),
+https://admintesting.herokuapp.com/users('index',views.index,name='index'),
+https://admintesting.herokuapp.com/userregistration('index',views.index,name='index'),
+https://admintesting.herokuapp.com/userlogin('index',views.index,name='index'),
+https://admintesting.herokuapp.com/seelost('index',views.index,name='index'),
+]
